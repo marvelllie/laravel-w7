9226bc09d24628044737968554985940b640c987
Satria Adi <satria@Satrias-MacBook-Pro.local>
2026-04-07 15:32:55 +0700

    database migration seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call(ProductCategorySeeder::class);
+        $this->call(ProductSeeder::class);
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
---
+++ database/seeders/ProductCategorySeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        DB::table('product_categories')->insert([
+            [
+                'name' => 'Electronics',
+                'description' => 'Devices and gadgets like smartphones, laptops, and TVs.',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'Clothing',
+                'description' => 'Apparel and fashion items for all seasons.',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'Books',
+                'description' => 'Fiction, non-fiction, educational, and more.',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'Home & Kitchen',
+                'description' => 'Appliances, cookware, and home essentials.',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'Sports & Outdoors',
+                'description' => 'Equipment and gear for sports and outdoor activities.',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+        ]);
+    }
+}
